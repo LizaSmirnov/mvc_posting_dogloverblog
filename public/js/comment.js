2f26ac5ab220e46bcf09778e4f2d7be4d4e74986
Liza Smirnov <krasnienkov@gmail.com>
2023-06-09 11:42:51 -0700

    everything running smoothly

--- public/js/comment.js
+++ public/js/comment.js
@@ -13,11 +13,12 @@ async function createComment(event) {
     console.log('createComment fired!')
     const body = document.querySelector('textarea[name="comment-body"]').value.trim();
     const name = document.querySelector('input[name="user-name"]').value;
-    console.log(body, name)
-    if (name) {
+    const post_id = document.querySelector('input[name="post-id"]').value;
+ 
+    if (body && name && post_id) {
         const response = await fetch('/api/comments', {
             method: 'POST',
-            body: JSON.stringify({ body, name }),
+            body: JSON.stringify({ body, name, post_id }),
             headers: {
                 'Content-Type': 'application/json',
             },
